34c0e3560e7d04585e8d22148d9120cdc1dfc37f
DuckySmacky <kriperoka@gmail.com>
2026-09-14 21:40:11 +0200

    Add build step for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,5 @@ set -euo pipefail
 cd "$REPO_PATH"
 git fetch origin master
 git reset --hard origin/master
+cargo build --release --manifest-path backend/Cargo.toml
 systemctl --user restart nikdor.xyz-backend.service
